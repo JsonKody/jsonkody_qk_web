99ceda33cb5e2d610406f5228de5afa9e5fbc1f1
JsonKody <daniel8cech@gmail.com>
2023-11-13 21:45:43 +0100

    saved & pushed by pnpm

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -64,11 +64,11 @@ export default component$(() => {
 });
 
 export const head: DocumentHead = {
-  title: "Welcome to Qwik",
+  title: "JsonKody",
   meta: [
     {
       name: "description",
-      content: "Qwik site description",
+      content: "JsonKody web implemented in Qwik.",
     },
   ],
 };
